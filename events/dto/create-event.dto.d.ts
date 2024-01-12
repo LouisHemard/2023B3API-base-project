58523e332ad34398b22fdda91931cb521aed3137
LouisHemard <92872441+LouisHemard@users.noreply.github.com>
2024-01-12 20:13:45 +0100

    reparation git

--- events/dto/create-event.dto.d.ts
+++
@@ -1,9 +0,0 @@
-import { EventStatus, EventType } from '../entities/event.entity';
-export declare class CreateEventDto {
-    id: string;
-    date: Date;
-    eventStatus?: EventStatus;
-    eventType: EventType;
-    eventDescription?: string;
-    userId: string;
-}
